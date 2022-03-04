d251c6f4cb12d9ce91df285edb27bb4a526c0a44
wei <firenba46@gmail.com>
2022-03-04 15:28:03 +0800

    Scoring for testing

---
+++ src/main/java/com/wei/Scoring.java
@@ -0,0 +1,12 @@
+package com.wei;
+
+public class Scoring {
+    public static void main(String[] args) {
+        String[] name = {"lisa", "jennie", "reyna", "john", "june"};
+        int[] english = {80, 55, 34, 91, 65};
+        int[] math = {66, 43, 78, 95, 84};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name[i] + "\t" + english[i] + "\t" + math[i]+"\t"+(english[i]+math[i])/2);
+        }
+    }
+}
