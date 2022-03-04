5e12a4a063537a87232a08d70d952d59d00c01c0
wei <firenba46@gmail.com>
2022-03-04 16:08:46 +0800

    ScoringNew for testing

---
+++ src/main/java/com/wei/ScoringNew.java
@@ -0,0 +1,10 @@
+package com.wei;
+
+public class ScoringNew {
+    public static void main(String[] args) {
+        Student lisa = new Student();
+        lisa.name = "Lisa";
+        lisa.english = 80;
+        lisa.math = 66;
+    }
+}
---
+++ src/main/java/com/wei/Student.java
@@ -0,0 +1,7 @@
+package com.wei;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+}
