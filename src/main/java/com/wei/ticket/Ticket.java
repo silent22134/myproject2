4190834d488a83d7a764aad962cce195b73cb8ce
silent22134 <firenba46@gmail.com>
2022-03-25 16:27:15 +0800

    ticket testing

--- src/main/java/com/wei/ticket/Ticket.java
+++ src/main/java/com/wei/ticket/Ticket.java
@@ -1,16 +1,13 @@
 package com.wei.ticket;
 
 public class Ticket {
-    static final int TAIPEI_STATION = 100;
-    static final int TAICHUNG_STATION = 200;
-    static final int KAOHSIUNG_STATION = 300;
+    public static final int TAIPEI_STATION = 100;
+    public static final int TAICHUNG_STATION = 200;
+    public static final int KAOHSIUNG_STATION = 300;
 
     int start;
     int destination;
     int price;
-    float off1 = 0.2f;
-    float off2 = 0.5f;
-    float off3 = 0.1f;
     public Ticket(int start,int destination,int price){
         this.start=start;
         this.destination=destination;
--- src/main/java/com/wei/ticket/TicketSale.java
+++ src/main/java/com/wei/ticket/TicketSale.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class TicketSale {
     public static void main(String[] args) {
         Ticket ticket = new Ticket(Ticket.TAIPEI_STATION,Ticket.KAOHSIUNG_STATION,);
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your start station");
+        System.out.println("Please enter your start station :(1.Taipei 2.Taichung 3.Kaohsiung)");
         int startStation = Integer.parseInt(scanner.next());
     }
 }
