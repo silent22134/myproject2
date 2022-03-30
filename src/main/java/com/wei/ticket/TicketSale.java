4a26e6f94c0efb45927e60249a06d63f3c5ec5b1
silent22134 <firenba46@gmail.com>
2022-03-30 22:09:18 +0800

    ticket work testing

--- src/main/java/com/wei/ticket/TicketSale.java
+++ src/main/java/com/wei/ticket/TicketSale.java
@@ -8,6 +8,32 @@ public class TicketSale {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your start station :(1.Taipei 2.Taichung 3.Kaohsiung)");
-        int startStation = Integer.parseInt(scanner.next());
+        int choice = Integer.parseInt(scanner.next());
+       Station start = null;
+       switch (choice){
+           case 1:
+           start = Station.TAIPEI_STATION;
+           break;
+           case 2:
+           start = Station.TAICHUNG_STATION;
+           break;
+           case 3:
+           start = Station.KAOHSIUNG_STATION;
+           break;
+       }
+        System.out.println("Please enter your destination station :(1.Taipei 2.Taichung 3.Kaohsiung)");
+        choice = Integer.parseInt(scanner.next());
+        Station destination = null;
+        switch (choice){
+            case 1:
+                destination = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                destination = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                destination = Station.KAOHSIUNG_STATION;
+                break;
+        }
     }
 }
