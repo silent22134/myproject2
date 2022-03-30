e5283a26702a26116891c05c721c432e010e71ef
silent22134 <firenba46@gmail.com>
2022-03-30 22:02:47 +0800

    ticket work testing

--- src/main/java/com/wei/ticket/Ticket.java
+++ src/main/java/com/wei/ticket/Ticket.java
@@ -5,15 +5,15 @@ public class Ticket {
     public static final int TAICHUNG_STATION = 200;
     public static final int KAOHSIUNG_STATION = 300;
 
-    int start;
-    int destination;
+    Station start;
+    Station destination;
     int price;
-    public Ticket(int start,int destination,int price){
+    public Ticket(Station start,Station destination){
         this.start=start;
         this.destination=destination;
-        this.price=price;
     }
     public void print(){
         System.out.println();
+
     }
 }
--- src/main/java/com/wei/ticket/TicketSale.java
+++ src/main/java/com/wei/ticket/TicketSale.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TicketSale {
     public static void main(String[] args) {
-        Ticket ticket = new Ticket(Ticket.TAIPEI_STATION,Ticket.KAOHSIUNG_STATION,);
+        Ticket ticket = new Ticket(Station.TAIPEI_STATION,Station.KAOHSIUNG_STATION);
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your start station :(1.Taipei 2.Taichung 3.Kaohsiung)");
