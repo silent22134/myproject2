4308c32db4a306c0c2968b4adf097076791e8193
silent <firenba46@gmail.com>
2022-04-01 16:28:40 +0800

    customer list<> test

--- src/main/java/com/wei/HelloWorld.java
+++ src/main/java/com/wei/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.wei;
 
+import java.util.ArrayList;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        ArrayList<Integer> list =new ArrayList<>();
+
         System.out.println("Hello World!");
     }
 
--- src/main/java/com/wei/sales/MemberLevel.java
+++ src/main/java/com/wei/sales/MemberLevel.java
@@ -1,18 +1,21 @@
 package com.wei.sales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberLevel {
     public static void main(String[] args) {
-        Customer customer1 = new Customer("0001",1200);
-        Customer customer2 = new Customer("0002",800);
-        Silver silver = new Silver("0003",2000);
-        Golden golden = new Golden("0004",5000);
-        Discount customer3 = new Discount("0006",900);
-        Platinum plantium = new Platinum("0007",1000);
-        customer1.print();
-        customer2.print();
-        silver.print();
-        golden.print();
-        customer3.print();
-        plantium.print();
+        List<Customer>customers = new ArrayList<>();
+        customers.add( new Customer("0001",1200));
+        customers.add( new Customer("0002",800));
+        customers.add( new Silver("0003",2000));
+        customers.add( new Golden("0004",5000));
+        customers.add( new Discount("0006",900));
+        for (int i = 0; i < 5; i++) {
+            customers.get(i).print();
+        }
+        for (Customer c: customers){
+            c.print();
+        }
     }
 }
